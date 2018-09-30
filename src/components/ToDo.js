2a3555a7f434663eaba6417dff111c889f7ae54e
Natalie Thomas <natalie.thomas.nmt@gmail.com>
2018-09-30 13:38:59 -0400

    Changed li to checkboxes and updated li values

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
  class ToDo extends Component {
    render() {
      return (
-       <li>I'm a to-do!</li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+      </li>
      );
    }
  }
